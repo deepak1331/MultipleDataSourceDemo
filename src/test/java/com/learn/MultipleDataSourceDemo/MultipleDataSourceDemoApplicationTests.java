6b6197a31e16389561311579f267ed502dd7fa33
Deepak Yadav <deepakyadav1331@gmail.com>
2025-09-01 16:38:41 +0530

    Initial commit

---
+++ src/test/java/com/learn/MultipleDataSourceDemo/MultipleDataSourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.MultipleDataSourceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDataSourceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
